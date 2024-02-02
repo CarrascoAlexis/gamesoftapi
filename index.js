2f5e96995f841e4218ec1afd0b1cb94f14d5285b
CarrascoAlexis <acarrasco@gaming.tech>
2024-02-02 19:16:51 +0100

    FIrst get request for global account get with firsts request args

--- index.js
+++ index.js
@@ -21,13 +21,51 @@ app.use(express.json())
 
 
 
+app.get("/users", (req, res) => {
 
+    if(req.body.filter == undefined)
+    {
+        db.query('SELECT * FROM account', (err, results) => {
+            if(err) res.json({"error": err})
+            else res.json(results)
+        })
+        return;
+    }
+    db.query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'account';", (err, results) => {
+        if(err)
+        {
+            res.json({"error": err})
+            return;
+        }
+        let filters = []
+        results.forEach(element => {
+            if(req.body.filter[element.COLUMN_NAME] != undefined)
+            {
+            filters[element.COLUMN_NAME] = req.body.filter[element.COLUMN_NAME]
+            }
+        })
+        console.log(filters)
+        res.json("aaaa")
+        db.query('SELECT * FROM account', (err, results) => {
+            if(err) res.json({"error": err})
+            else res.json(results)
+        })
+    })
+});
+
+app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    db.query('SELECT * FROM account WHERE id = ?', [id], (err, results) => {
+        if(err) res.json({"error": err})
+        else res.json(results)
+    })
+})
 
 
 
 // API Main page (useless)
 app.get('/', (req, res) => {
-    res.json("coucou")
+    res.send(`Succesfully connected to ${db.config.database} Database.`)
 })
 
 app.listen(PORT, () => {
